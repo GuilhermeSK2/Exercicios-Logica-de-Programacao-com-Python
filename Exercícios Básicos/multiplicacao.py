1b96ae056d622094baa14e6d962a683678dd72a5
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-05-03 23:26:33 -0300

    First Commit

---
+++ Exercícios Básicos/multiplicacao.py
@@ -0,0 +1,7 @@
+#Programa que recebe 2 valores, realiza a multiplicação e retorna o resultado
+
+print("Vamos realizar a multplicação de dois números!")
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite um segundo número: "))
+multiplicacao = (numero1 * numero2)
+print("O resultado da multiplicação dos dois números é: ", multiplicacao)
---
+++ Exercícios Básicos/subtracao.py
@@ -0,0 +1,7 @@
+#Programa que recebe 2 valores, realiza a subtração e retorna o resultado
+
+print("Vamos realizar a subtração de dois números!")
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite um segundo número: "))
+subtracao = (numero1 - numero2)
+print("O resultado da subtração dos dois números é: ", subtracao)
