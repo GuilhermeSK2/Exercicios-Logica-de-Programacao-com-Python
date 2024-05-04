757b9d08b6e7b971d9607e7aff2c35f55d59c379
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-05-04 09:58:22 -0300

    Delete Exercícios Básicos/divisao.py

--- Exercícios Básicos/divisao.py
+++
@@ -1,7 +0,0 @@
-#Programa que recebe 2 valores, realiza a divisão e retorna o resultado
-
-print("Vamos realizar a divisão de dois números!")
-numero1 = int(input("Digite um numero: "))
-numero2 = int(input("Digite um segundo número: "))
-divisao = (numero1 / numero2)
-print("O resultado da divisao dos dois números é: ", divisao)
