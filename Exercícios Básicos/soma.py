96a8f3e094347abb430b5d5d8c67ad2c5bfe69a4
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-05-03 23:17:09 -0300

    First Commit

---
+++ Exercícios Básicos/soma.py
@@ -0,0 +1,7 @@
+#Programa que recebe 2 valores, realiza a soma e retorna o resultado
+
+print("Vamos realizar a soma de dois números!")
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite um segundo número: "))
+soma = (numero1 + numero2)
+print("O resultado da soma dos dois números é: ", soma)
