a3007fdbbceb1ca12c12a65cb4523c64f21215ee
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-05-05 08:01:26 -0300

    Update ex-06-AreaQuadrado.py

--- Apostila Faccat/Página 4/ex-06-AreaQuadrado.py
+++ Apostila Faccat/Página 4/ex-06-AreaQuadrado.py
@@ -3,7 +3,7 @@
 # Versão 1.0
 # Excercicio de Lógica de programação com a Linguagem de Programação Python
 
-# Algoritmo  para  ler  as  dimensões  de  um  quadrado  (base  e  altura),  calcular  e  escrever  a área do quadrado.
+# Algoritmo  para  ler  as  dimensões  de  um  quadrado  (qualquer um dos lados),  calcular  e  escrever  a área do quadrado.
 
 print("Vamos calcular a área do quadrado!")
 lado = int(input("Insira o valor de um dos lados:"))
