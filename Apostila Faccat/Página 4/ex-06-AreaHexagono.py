30d89efa99bef08f3dd25eeb0d3e331a7384a733
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-05-11 09:55:52 -0300

    Update ex-06-AreaHexagono.py

--- Apostila Faccat/Página 4/ex-06-AreaHexagono.py
+++ Apostila Faccat/Página 4/ex-06-AreaHexagono.py
@@ -3,11 +3,13 @@
 # Versão 1.0
 # Excercicio de Lógica de programação com a Linguagem de Programação Python
 
+import math
+
 # Algoritmo  para  ler  as  dimensões  de  um  hexagono  (lado),  calcular  e  escrever  a área do hexagono.
 
 print("Vamos calcular a área de um hexagono!")
-lado = int(input("Digite o cumprimento de um dos lados do hexagono:"))
+lado = float(input("Digite o cumprimento de um dos lados do hexagono:"))
 
-area = (3 * (3 ** 0.5) * (lado ** 2)) / 2
+area = ((3 * pow (lado,2) * math.sqrt(3)) / 2)
 
-print("A área do hexagono é", area)
+print("A área do hexagono é", round(area,2), " m²")
