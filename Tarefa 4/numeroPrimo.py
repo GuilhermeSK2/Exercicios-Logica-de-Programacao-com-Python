d21a2859e9b1c54d76097c55fc9072b902085efe
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-05-25 13:02:22 -0300

    Update Aula 5

---
+++ Tarefa 4/numeroPrimo.py
@@ -0,0 +1,23 @@
+# Curso Básico de Python
+# Nome do Desenvolvedor: Guilherme Freitas dos Santos
+# Versão 1.0
+# Excercicio de Lógica de programação com a Linguagem de Programação Python
+
+#Faça um programa que receba um número inteiro e retorne se é primo ou não.
+
+#Um número inteiro é primo quando ele é divisível apenas por 1 e por ele mesmo.
+
+#Exemplo: 2, 3, 5, 7, 11, 13, 17, 19, 23
+
+numero =int(input("Digite um número inteiro para saber se é primo :"))
+
+if numero<=1:
+    print("O número não é primo")
+elif numero%2==0 and numero!=2:
+    print("O número não é primo")
+elif numero%3==0 and numero!=3 or numero%5==0 and numero!=5 or numero%7==0 and numero!=7:
+  print("O número não é primo")
+
+else:
+    print("O número é primo")
+
---
+++ Tarefa 7/acumuladora.py
@@ -0,0 +1,13 @@
+# Curso Básico de Python
+# Nome do Desenvolvedor: Guilherme Freitas dos Santos
+# Versão 1.0
+# Excercicio de Lógica de programação com a Linguagem de Programação Python
+
+contadora = 1
+acumuladora = 0
+
+while (contadora<101):
+    acumuladora = acumuladora + contadora
+    contadora += 1
+    
+print("A soma total correspondente a: ", acumuladora)
